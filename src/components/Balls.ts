f25263ec051319338a38111a1f09a1e216d27f06
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-24 11:59:26 +0100

    button click works

--- src/components/Balls.ts
+++ src/components/Balls.ts
@@ -1,7 +1,7 @@
 export const Balls = (colors: string[]) => {
 let html = '';
 for (const color of colors) {
-	html += `<div class="w-16 h-16 flex justify-center items-center rounded-[50%] mb-3" style="background-color: ${color}">${color}</div>`;
+	html += `<div class="w-24 h-24 flex justify-center items-center rounded-[50%] mb-3" style="background-color: ${color}">${color}</div>`;
 }
 	return `${html}`
 }
--- src/components/ExampleButtonToggle.ts
+++ src/components/ExampleButtonToggle.ts
@@ -1,15 +1,21 @@
+import { Balls } from "./Balls";
+
 export const ExampleButtonToggle = () => {
 	return `
-	<button class="btnToggle btn-primary">Toggle Message</button>
+	<button class="btnToggle btn-primary">Toggle Balls</button>
+	<div class="balls mt-4">
+	${Balls(['lightgreen', 'goldenrod'])}
+	</div>
 	`
 }
 
 setTimeout(() => {
 	const btnToggleElem = document.querySelector<HTMLButtonElement>('.btnToggle');
+	const ballsElem = document.querySelector<HTMLDivElement>('.balls');
 
-	if (btnToggleElem) {
+	if (btnToggleElem && ballsElem) {
 		btnToggleElem.addEventListener('click', () => {
-			alert('it was clicked')
+			ballsElem.style.display = ballsElem.style.display === 'none' ? 'block' : 'none';
 		})
 	}
 })
