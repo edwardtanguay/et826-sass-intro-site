43903c1655bf3c490cd8796288ba0bb0f99dab81
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-24 11:08:04 +0100

    sass mixin in

---
+++ src/components/Header.ts
@@ -0,0 +1,7 @@
+
+const note = ' - the first site';
+const appTitle = 'Our Sass Site' + note;
+
+export const Header = () => {
+	return `<h1>${appTitle}</h1>`
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
+import { Header } from './components/Header';
 import './style.scss'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
-<h1>Sass Site</h1>
+${Header()}
 <p>testing</p>
+<button class="btn-primary">Press Here</button>
+<button class="btn-warning">Press Here</button>
+<button class="btn-danger">Press Here</button>
 `;
